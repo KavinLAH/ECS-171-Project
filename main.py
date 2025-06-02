a140df98cc31f21ab521607c143b2774cfe32837
JonathanPLev <jplevitsky@ucdavis.edu>
2025-06-01 22:07:54 -0700

    TODO: set up main

---
+++ main.py
@@ -0,0 +1,7 @@
+import pandas as pd
+import numpy as np
+
+# for running on two players by being given their names: only from the NBA
+
+# TODO: set up NBA data processing inside this file to identify stats of the two players given, compare the two, and then output the winning player. 
+# also error handling (player dont exist, bad spelling, etc.)
